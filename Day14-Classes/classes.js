c6df2257b7e3d5f8a0d9adf180672241aef407fb
Sandeep <sandeepwadhawan2001@gmail.com>
2024-07-27 00:37:21 +0530

    Added optional activity related to private fields in Class

--- Day14-Classes/classes.js
+++ Day14-Classes/classes.js
@@ -124,3 +124,53 @@ console.log(anotherPerson.fullName); //John Doe
 
 anotherPerson.fullName = "James Phoenix";
 console.log(`The updated name becomes ${anotherPerson.fullName}`);
+
+/* Activity 5 : Private Fields (Optional) */
+
+// Task 9 : Define a class Account with private fields for balance and a method to deposit and withdraw money. Ensure that the balance can only be updated through these methods.
+
+class Account {
+    // Private field named 'balance'
+    #balance;
+
+    constructor(initialBalance = 0) {
+        this.#balance = initialBalance;
+    }
+
+    deposit(amount) {
+        if (amount > 0) {
+            this.#balance += amount;
+        } else {
+            console.error(`Deposit amount must be positive`);
+        }
+    }
+
+    withdraw(amount) {
+        if (amount > 0) {
+            if (amount <= this.#balance) {
+                this.#balance -= amount;
+            } else {
+                console.error(`Insufficient Withdrawal amount`);
+            }
+        } else {
+            console.error(`Withdrawal amount must be positive`);
+        }
+    }
+
+    getBalance() {
+        return `The total balance is ${this.#balance}`;
+    }
+}
+
+// Task 10 : Create an instance of Amount class and test the deposit and withdraw methods.
+
+const acc = new Account(180);
+
+acc.deposit(50);
+console.log(acc.getBalance()); // The total balance is 230
+
+acc.withdraw(40);
+console.log(acc.getBalance()); // The total balance is 190
+
+acc.withdraw(300); // Insufficient Withdrawal amount
+console.log(acc.getBalance()); // The total balance is 190
