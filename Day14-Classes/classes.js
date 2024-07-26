8067ca44b7cedeb74fdb9a1a18c6741201e37184
Sandeep <sandeepwadhawan2001@gmail.com>
2024-07-27 00:08:54 +0530

    Day-14 task related to Classes in Javascript is completed

---
+++ Day14-Classes/classes.js
@@ -0,0 +1,126 @@
+/* Activity 1 : Class Definition */
+
+// Task 1 : Create a class Person and add props, methods to it
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        return `Hello ${this.name}, Good Morning dear!!`;
+    }
+
+    static greetMssg() {
+        return `Good evening friends! Want some coffee?`;
+    }
+}
+
+const p = new Person("Sandeep", 22);
+console.log(p.greet()); //Hello Sandeep, Good Morning dear!!
+
+
+// Task 2 : Add a method to the Person class that updates the age property
+
+console.log(`The present age is ${p.age}`); //The present age is 22
+
+Person.prototype.updateAge = function () {
+    this.age += 5;
+    return `My updated age becomes ${this.age}`;
+}
+
+console.log(p.updateAge()); //My updated age becomes 27
+
+
+/* Activity 2 : Class Inheritance */
+
+// Task 3 : Define a class Student that extends the Person class.
+
+class Student extends Person {
+    static studentCount = 0;
+
+    constructor(name, age, studentID) {
+        super(name, age);
+        this.studentID = studentID;
+        Student.studentCount++;
+    }
+
+    getStudentId() {
+        return `The id of student is ${this.studentID}`;
+    }
+
+    greet() {
+        return `Hello ${this.name}, your id is ${this.studentID}. Good to see you again!!`;
+    }
+}
+
+const student = new Student('John', 20, '123');
+console.log(student.getStudentId()); //The id of student is 123
+
+// Task 4 : Override the greeting method in Student class
+
+const person = new Person('Ankit', 25);
+console.log(person.greet()); //Hello Ankit, Good Morning dear!!
+
+const stu = new Student('Amar', 23, '100');
+console.log(stu.greet()); //Hello Amar, your id is 100. Good to see you again!! (Overriden method)
+
+/* 
+
+Note : 
+
+- When greet is called on an instance of Person, it uses the Person class's greet method.
+
+- When greet is called on an instance of Student, it uses the Student class's greet method, which overrides the one from Person.
+
+*/
+
+/* Activity 3: Static Methods and Properties */
+
+// Task 5 : Add a static method to Person class
+
+const mssg = Person.greetMssg();
+console.log(mssg); //Good evening friends! Want some coffee?
+
+// Task 6 : Add a static property to the Student class to keep track of the number of students created. Increment this property in the constructor and log the total number of students.
+
+const student1 = new Student('Karan', 28, '4352');
+const student2 = new Student('Nijjar', 23, '8331');
+const student3 = new Student('Hargun', 30, '7065');
+
+const totalStudents = Student.studentCount;
+console.log(`The total number of students are ${totalStudents}`); //The total number of students are 5
+
+/* Activity 4 : Getters and Setters */
+
+class Person2 {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    set fullName(name) {
+        const [ firstName, lastName ] = name.split(' ');
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+// Task 7 : Add a getter method to Person2 class
+
+const per = new Person2("Sandeep", "Wadhawan");
+console.log(`The full name is ${per.fullName}`); //The full name is Sandeep Wadhawan
+
+
+// Task 8 : Add a setter method to Person2 class to update the name properties
+
+const anotherPerson = new Person2("John", "Doe");
+console.log(anotherPerson.fullName); //John Doe
+
+anotherPerson.fullName = "James Phoenix";
+console.log(`The updated name becomes ${anotherPerson.fullName}`);
